199ff7839c6356c443f153f0a5d1932b949652c6
TrifanTamara <trifantm@yahoo.com>
2018-04-16 17:52:10 +0300

    Quizz, Vocabular, Lesson templates added

---
+++ SourceCode/Foxy/Data.Domain/Entities/TemplateItems/VocabularRelationship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Domain.Entities.TemplateItems
+{
+    public class VocabularRelationship
+    {
+        private VocabularRelationship()
+        {
+            // EF Core    
+        }
+
+        public Guid Id { get; private set; }
+        public VocabularTemplate MainItem { get; private set; }
+        public VocabularTemplate ContainedItem { get; private set; }
+
+        public static VocabularRelationship Create(VocabularTemplate main, VocabularTemplate contained)
+        {
+            var instance = new VocabularRelationship() { Id = Guid.NewGuid() };
+            instance.Update(main, contained);
+            return instance;
+        }
+
+        public void Update(VocabularTemplate main, VocabularTemplate contained)
+        {
+            MainItem = main;
+            ContainedItem = contained;
+        }
+    }
+}
