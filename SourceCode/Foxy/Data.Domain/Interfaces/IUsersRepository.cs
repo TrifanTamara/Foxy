7f4dd623ac70e90cced0f5be618a311ac1a1622b
TrifanTamara <trifantm@yahoo.com>
2018-04-17 16:11:17 +0300

    user controller + db

--- SourceCode/Foxy/Data.Domain/Interfaces/IUsersRepository.cs
+++ SourceCode/Foxy/Data.Domain/Interfaces/IUsersRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Data.Domain.Entities;
 
 namespace Data.Domain.Interfaces
 {
-    public interface IUsersRepository
+    public interface IUsersRepository : IGenericRepository<User>
     {
     }
 }
