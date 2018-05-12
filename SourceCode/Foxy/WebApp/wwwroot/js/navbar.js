c192c3e732c468f5a472d97fba930872b867e6e5
TrifanTamara <trifantm@yahoo.com>
2018-05-12 22:32:35 +0300

    navbar changes

--- SourceCode/Foxy/WebApp/wwwroot/js/navbar.js
+++ SourceCode/Foxy/WebApp/wwwroot/js/navbar.js
@@ -1,10 +1,18 @@
 ﻿$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
     var scrollTop = 0;
+    if (window.innerWidth < 768)
+    {
+        $('#main-navbar').removeClass('class-myNav');
+        $('#main-navbar').removeClass('scrolled-nav');
+    }
+
     $(window).scroll(function () {
         scrollTop = $(window).scrollTop();
         $('.counter').html(scrollTop);
         if (window.innerWidth >= 768) {
             $('#main-navbar').addClass('class-myNav');
+            $('.menu-drop-main').removeClass('class-myNav');
             if (scrollTop >= 100) {
                 $('#main-navbar').addClass('scrolled-nav');
             } else if (scrollTop < 100) {
@@ -16,4 +24,13 @@
 
     });
 
+});
+
+
+$(window).resize(function () {
+    if (window.innerWidth < 768) {
+        $('#main-navbar').removeClass('class-myNav');
+        $('#main-navbar').removeClass('scrolled-nav');
+    }
+    
 });
