cc94f0bf7285f728e5b1152e2a2aaf8a95d54e74
TrifanTamara <trifantm@yahoo.com>
2018-06-06 00:37:36 +0300

    review page front-end updated

---
+++ SourceCode/Foxy/WebApp/wwwroot/js/review.js
@@ -0,0 +1,11 @@
+﻿jQuery(document).ready(function () {
+    $('#see-answer').hide();
+
+    $("#eye-button").click(function (e) {
+        if ($('#see-answer').is(':visible')) {
+            $('#see-answer').hide();
+        } else {
+            $('#see-answer').show();
+        }
+    });
+});
