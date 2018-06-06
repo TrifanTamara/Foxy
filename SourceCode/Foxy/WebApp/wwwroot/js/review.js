0be65d7fdb3cdb72a5be86864ae541e8b4df8ea4
TrifanTamara <trifantm@yahoo.com>
2018-06-06 17:13:37 +0300

    added kana input

--- SourceCode/Foxy/WebApp/wwwroot/js/review.js
+++ SourceCode/Foxy/WebApp/wwwroot/js/review.js
@@ -8,4 +8,52 @@
             $('#see-answer').show();
         }
     });
+
+    var textInput = document.getElementById('wanakana-input');
+    wanakana.bind(textInput, /* options */); // uses IMEMode with toKana() as default
+    // to remove event listeners: wanakana.unbind(textInput);
+
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (validateForm() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
 });
+
+function validateForm() {
+    var result = true;
+    var meaning = document.forms["review-form"]["meaning-input"].value;
+    var reading = document.forms["review-form"]["reading-input"].value;
+
+    //if (!wanakana.isKana(reading)) {
+    //    result = false;
+    //    $("#div-validate-reading").text("This field should contain only kana characters");
+    //} else
+    //if (reading == "") {
+        result = false;
+        $("#div-validate-reading").text("This field should not be empty");
+//    }
+
+    if (!wanakana.isKana(reading)) {
+        result = false;
+        $("#div-validate-reading").text("This field should contain only kana characters");
+    } 
+
+    if (meaning == "") {
+        result = false;
+        $("#div-validate-meaning").text("This field should not be empty");
+    }
+    return result;
+}
